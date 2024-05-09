0e88ca08daf51226de663fd5cfb3165cd9fec6ff
a3802 <253092329@qq.com>
2024-05-10 02:06:43 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+export default {
+
+  // 系统名称
+  name: "口袋折扣-优惠共享",
+
+  /**
+   * 后端api地址 (必填; 斜杠/结尾; 请确保能访问)
+   * 例如: https://www.你的域名.com/index.php?s=/api/
+   */
+  apiUrl: "https://devtgqy.yueyueyouqian.cn/api/",
+
+  /**
+   * 是否启用商城设置缓存
+   * 将减少用户端重复请求; 正式运营时请设为true, 开启后商城设置同步前端需10分钟缓存
+   */
+  enabledSettingCache: true
+
+}
---
+++ pages/common/minitrade.vue
@@ -0,0 +1,43 @@
+<template>
+	<view></view>
+</template>
+
+<script>
+import { urlEncode } from '../../utils/util';
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		
+		onLoad(item){
+			console.log(item);
+			let urlParams = Object.keys(item).map(key => key + '=' + encodeURIComponent(item[key]).replace(/%20/g,"+")).join('&');
+
+			my.tradePay({
+			  orderStr: urlParams,
+			  success: (res) => {
+				  
+				my.alert({
+				  content: JSON.stringify(res),
+				});
+				console.log(res);
+				my.exitMiniProgram();
+			  },
+			  fail: (res) => {
+				my.alert({
+				  content: JSON.stringify(res),
+				});
+			  }
+			});			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
